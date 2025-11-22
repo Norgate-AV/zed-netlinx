246fc06354ff2e339a73af0c123cb01c1bfe8133
Damien Butt <22627489+damienbutt@users.noreply.github.com>
2025-11-22 02:28:01 +0000

    feat: update outline.scm

--- languages/netlinx/outline.scm
+++ languages/netlinx/outline.scm
@@ -1,10 +1,29 @@
+(
+    [
+        (program_name)
+        (module_name)
+    ] @context
+) @item
+
 (preproc_define
     (preproc_define_keyword) @context
-    name: (_) @name) @item
+    name: (_) @name
+) @item
+
+(preproc_include
+    (preproc_include_keyword) @context
+    path: (_) @name
+) @item
+
+(preproc_warn
+    (preproc_warn_keyword) @context
+    message: (_) @name
+) @item
 
 (struct_specifier
     (struct_keyword) @context
-    name: (_) @name) @item
+    name: (_) @name
+) @item
 
 (define_function
     (define_function_keyword) @context
@@ -14,6 +33,34 @@
         parameters: (parameter_list
             "(" @context
             ")" @context))
+) @item
+
+(section
+    (_) @name
+) @item
+
+(define_module
+    (define_module_keyword) @context
+    (module_definition
+        module_name: (_) @name
+        instance_name: (_) @context
+        parameters: (argument_list
+            "(" @context
+            ")" @context))
+) @item
+
+(
+    [
+        (timeline_event_declarator)
+        (button_event_declarator)
+        (channel_event_declarator)
+        (level_event_declarator)
+        (custom_event_declarator)
+        (data_event_declarator)
+    ] @context
+    body: (_
+        "{" @context
+        "}" @context)
 ) @item
 
 (comment) @annotation
